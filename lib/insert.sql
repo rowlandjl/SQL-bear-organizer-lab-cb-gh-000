feef12d3e850bb8737882a0f1827a42dd3024a60
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-24 00:45:24 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ('Mr. Chocolate', 4, 'M', 'Brown', 'Sugary Sweet', 'Y');
