1c574b3c13cc68601ba1d8f23450217393f3fc71
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-24 00:58:45 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Mr. Chocolate', 4, 'M', 'Brown', 'Sugary Sweet', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'Brown', 'Sugary Sweet', true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ('Rowdy', 2, 'M', 'Reddish-brown', 'Hyper', true);
